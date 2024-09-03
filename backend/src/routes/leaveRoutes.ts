0ea78a5f8ae03104e90ec5b0130c84d50d6eaeca
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-09-03 12:37:52 +0300

    leave request done

--- backend/src/routes/leaveRoutes.ts
+++ backend/src/routes/leaveRoutes.ts
@@ -3,13 +3,29 @@ import auth from "../middleware/auth.js";
 import isAdmin from "../middleware/isAdmin.js";
 
 import leaveController from "../controllers/leaveController.js";
+import errorHandler from "../utils/error.js";
 
 const router = Router();
 
-router.post("/", auth, isAdmin, leaveController.leaveRequest);
-router.patch("/:id", auth, isAdmin, leaveController.updateLeaveRequest);
-router.get("/", auth, leaveController.getAllLeaveRequest);
-router.get("/:id", auth, isAdmin, leaveController.getLeaveRequestById);
-router.delete("/:id", auth, isAdmin, leaveController.removeLeaveRequest);
+router.post("/", auth, errorHandler(leaveController.leaveRequest));
+router.patch(
+  "/:id",
+  auth,
+  isAdmin,
+  errorHandler(leaveController.updateLeaveRequest)
+);
+router.get("/", auth, errorHandler(leaveController.getAllLeaveRequest));
+router.get(
+  "/:id",
+  auth,
+  isAdmin,
+  errorHandler(leaveController.getLeaveRequestById)
+);
+router.delete(
+  "/:id",
+  auth,
+  isAdmin,
+  errorHandler(leaveController.removeLeaveRequest)
+);
 
 export default router;
